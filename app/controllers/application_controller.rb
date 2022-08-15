b65d3d489c34731341332095f0836018e36b3374
mluthfij <luthfujuliansyah48@gmail.com>
2022-08-15 18:08:45 +0700

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
+
    protected
-      def configure_permitted_parameters
-         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
-         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :current_password)}
-      end
+
+   def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      # devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :username, :email, :password, :password_confirmation, :remember_me, :current_password)}
+
+      # devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
+      # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :current_password)}
+   end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,33 @@ class User < ApplicationRecord
          # Validation 
          validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
               file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
+
+  attr_writer :login
+  validate :validate_username
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value",
+      { value: login.downcase}]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  # def username
+  #   #email = john@example.com -> ['john']
+  #   return self.email.split('@')[0].capitalize
+  # end
+              
 end
