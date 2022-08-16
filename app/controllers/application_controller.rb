9e386864316997e9970c25a2b18d67c610e4309b
mluthfij <luthfujuliansyah48@gmail.com>
2022-08-16 07:09:18 +0700

    production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,5 @@ class ApplicationController < ActionController::Base
 
       # devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
       # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :current_password)}
-   end  
+   end
 end
